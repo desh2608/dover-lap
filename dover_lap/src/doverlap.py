fixed bug in unsorted hungarian mapping rank weighting

dover_lap/src/doverlap.py
@@ -23,7 +23,7 @@ class DOVERLap:
     ) -> List[List[Turn]]:
 
         # Label mapping stage
-        mapped_turns_list, ranks = LabelMapping.get_mapped_turns_list(
+        mapped_turns_list, weights = LabelMapping.get_mapped_turns_list(
             turns_list,
             file_id,
             method=label_mapping,
@@ -33,17 +33,27 @@ class DOVERLap:
 
         # Compute weights based on rank
         if weight_type == "rank":
+            ranks = cls.__get_ranks(weights)
             weights = cls.__compute_weights(ranks, dover_weight)
-        else:
+        elif weight_type == "custom":
             assert isinstance(custom_weight, list)
             weights = np.array([float(x) for x in custom_weight])
+        elif weight_type == "norm":
+            weights /= np.linalg.norm(weights, ord=1)  # use normalized weights
 
         # Label voting stage
         combined_turns_list = LabelVoting.get_combined_turns(
             mapped_turns_list, file_id, tie_breaking, weights
         )
-
         return combined_turns_list
+
+    def __get_ranks(weights: np.array) -> np.array:
+
+        weights /= np.linalg.norm(weights, ord=1)
+        temp = weights.argsort()
+        ranks = np.empty_like(temp)
+        ranks[temp] = np.arange(len(weights)) + 1
+        return ranks
 
     def __compute_weights(ranks: np.array, weight: float) -> np.array:
 

dover_lap/src/label_mapping.py
@@ -55,13 +55,4 @@ class LabelMapping:
                     )
             mapped_turns_list.append(mapped_turns)
 
-        ranks = cls.__get_ranks(weights)
-        return mapped_turns_list, ranks
-
-    def __get_ranks(weights: np.array) -> np.array:
-
-        weights /= np.linalg.norm(weights, ord=1)
-        temp = weights.argsort()
-        ranks = np.empty_like(temp)
-        ranks[temp] = np.arange(len(weights)) + 1
-        return ranks
+        return mapped_turns_list, weights

dover_lap/src/mapping/hungarian.py
@@ -25,13 +25,11 @@ class HungarianMap:
         """
         self.turns_list = turns_list
 
+        weights = self.__compute_weights()
+        self.sorted_turns_list = self.turns_list
         if self.sort_first:
-            weights = self.__compute_weights()
             sorted_idx = weights.argsort().tolist()
             self.sorted_turns_list = [self.turns_list[i] for i in sorted_idx]
-        else:
-            weights = np.ones((len(self.turns_list)))
-            self.sorted_turns_list = self.turns_list
 
         cur_turns = self.sorted_turns_list[0]
         self.global_mapping = dict()