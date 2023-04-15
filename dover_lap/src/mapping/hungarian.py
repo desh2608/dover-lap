a7d91eb399bcdff285decd4e4cff9b06935eff08
Desh Raj <r.desh26@gmail.com>
2023-04-15 15:17:05 -0400

    use spyder label mapping

--- dover_lap/src/mapping/hungarian.py
+++ dover_lap/src/mapping/hungarian.py
@@ -1,12 +1,10 @@
 import numpy as np
-import sys
 
 from typing import List, Dict, Tuple, Optional
-from scipy.optimize import linear_sum_assignment
 
-from dover_lap.libs.utils import groupby, info
+from dover_lap.libs.utils import groupby
 from dover_lap.libs.turn import Turn, merge_turns
-import spyder
+from spyder import DER
 
 from .map_utils import *
 
@@ -24,7 +22,7 @@ class HungarianMap:
         """
         self.turns_list = turns_list
 
-        weights = self.__compute_weights()
+        weights = self._compute_weights()
         self.sorted_turns_list = self.turns_list
         if self.sort_first:
             sorted_idx = weights.argsort().tolist()
@@ -35,15 +33,15 @@ class HungarianMap:
 
         for i in range(1, len(self.sorted_turns_list)):
             next_turns = self.sorted_turns_list[i]
-            local_mapping = self.__map_pair(cur_turns, next_turns)
-            cur_turns = self.__merge_pair(cur_turns, next_turns, local_mapping)
-            self.__update_global_map(local_mapping)
+            local_mapping = self._map_pair(cur_turns, next_turns)
+            cur_turns = self._merge_pair(cur_turns, next_turns, local_mapping)
+            self._update_global_map(local_mapping)
 
-        if not self.__validate_global_mapping():
+        if not self._validate_global_mapping():
             raise Exception("Some speakers have not been mapped")
         return self.global_mapping, weights
 
-    def __validate_global_mapping(self) -> bool:
+    def _validate_global_mapping(self) -> bool:
         for i, turns in enumerate(self.sorted_turns_list):
             groups = {
                 key: list(group)
@@ -54,7 +52,7 @@ class HungarianMap:
                     return False
         return True
 
-    def __compute_weights(self) -> np.ndarray:
+    def _compute_weights(self) -> np.ndarray:
         N = len(self.turns_list)
         DERs = np.zeros((N, N))
         for i in range(N):
@@ -67,63 +65,28 @@ class HungarianMap:
                     (turn.speaker_id, turn.onset, turn.offset)
                     for turn in self.turns_list[j]
                 ]
-                DERs[i, j] = spyder.DER(ref, hyp).der
+                DERs[i, j] = DER(ref, hyp).der
         mean_ders = DERs.mean(axis=0)
         return -1 * mean_ders
 
-    def __map_pair(
+    def _map_pair(
         self, ref_turns: List[Turn], sys_turns: List[Turn]
     ) -> Dict[Tuple[int, str], int]:
-        ref_groups = {
-            key: list(group)
-            for key, group in groupby(ref_turns, lambda x: x.speaker_id)
-        }
-        sys_groups = {
-            key: list(group)
-            for key, group in groupby(sys_turns, lambda x: x.speaker_id)
-        }
-        ref_keys = sorted(ref_groups.keys())
-        sys_keys = sorted(sys_groups.keys())
-        M, N = len(ref_keys), len(sys_keys)
-        cost_matrix = np.zeros((M, N))
-        for i, ref_spk_id in enumerate(ref_keys):
-            cur_row = []
-            ref_spk_turns = ref_groups[ref_spk_id]
-            for j, sys_spk_id in enumerate(sys_keys):
-                sys_spk_turns = sys_groups[sys_spk_id]
-                total_overlap = compute_spk_overlap(ref_spk_turns, sys_spk_turns)
-                cost_matrix[i, j] = -1 * total_overlap
+        ref = [(turn.speaker_id, turn.onset, turn.offset) for turn in ref_turns]
+        sys = [(turn.speaker_id, turn.onset, turn.offset) for turn in sys_turns]
+        metrics = DER(ref, sys)
+        ref_map = metrics.ref_map
+        sys_map = metrics.hyp_map
 
-        row_ind, col_ind = linear_sum_assignment(cost_matrix)
-
-        # Keep track of remaining row or col indices
-        row_indices_remaining = list(range(M))
-        col_indices_remaining = list(range(N))
         label_mapping = {}
-
-        for i in range(len(row_ind)):
-            label_mapping[(0, ref_keys[row_ind[i]])] = i
-            row_indices_remaining.remove(row_ind[i])
-            label_mapping[(1, sys_keys[col_ind[i]])] = i
-            col_indices_remaining.remove(col_ind[i])
-
-        next_label = i + 1
-
-        # Assign labels to remaining row indices
-        while len(row_indices_remaining) != 0:
-            label_mapping[(0, ref_keys[row_indices_remaining[0]])] = next_label
-            next_label += 1
-            del row_indices_remaining[0]
-
-        # Assign labels to remaining col indices
-        while len(col_indices_remaining) != 0:
-            label_mapping[(1, sys_keys[col_indices_remaining[0]])] = next_label
-            next_label += 1
-            del col_indices_remaining[0]
+        for k, v in ref_map.items():
+            label_mapping[(0, k)] = v
+        for k, v in sys_map.items():
+            label_mapping[(1, k)] = v
 
         return label_mapping
 
-    def __merge_pair(
+    def _merge_pair(
         self,
         ref_turns: List[Turn],
         sys_turns: List[Turn],
@@ -148,7 +111,7 @@ class HungarianMap:
         all_turns = merge_turns(ref_turns_mapped + sys_turns_mapped)
         return all_turns
 
-    def __update_global_map(self, local_map: Dict[Tuple[int, str], int]) -> None:
+    def _update_global_map(self, local_map: Dict[Tuple[int, str], int]) -> None:
         if not self.global_mapping:
             self.global_mapping = local_map.copy()
             return
